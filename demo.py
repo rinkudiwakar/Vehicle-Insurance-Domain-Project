98d58d9efca7c297ff32ecd2a881501498f282ec
Rinku Diwakar <prateek.diwakarr@gmail.com>
2026-02-11 22:28:00 +0530

    completed the data ingestion part

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
-from src.logger import logging
+# from src.logger import logging
 
-from src.exception import MyException
+# from src.exception import MyException
 
-import sys
+# import sys
 
-try:
-  a=1+'Z'
-except Exception as e:
-  logging.info(e)
-  raise MyException(e,sys) from e
+print("This is a demo file to test the code execution")
+
+from src.components.data_ingestion import DataIngestion
+data_ingestion=DataIngestion()
+data_ingestion.export_data_into_feature_store()
